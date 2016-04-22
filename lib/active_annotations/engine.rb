05dbb38862b393c7636c1243529895319d9ba89c
Michael B. Klein <mbklein@gmail.com>
2016-04-22 01:41:04 -0500

    Push migration into main app namespace

--- lib/active_annotations/engine.rb
+++ lib/active_annotations/engine.rb
@@ -2,6 +2,13 @@ if defined?(Rails)
   module ActiveAnnotations  
     class Engine < Rails::Engine
       config.autoload_paths += Dir["#{config.root}/app/models/**/"]
+      initializer :append_migrations do |app|
+        unless app.root.to_s.match root.to_s
+          config.paths["db/migrate"].expanded.each do |expanded_path|
+            app.config.paths["db/migrate"] << expanded_path
+          end
+        end
+      end
     end
   end
 end
